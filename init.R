c93f3bd5f81b749ecb9cc428b369beb52a7bf2a1
Shutong-Song <jayssong001@gmail.com>
2025-06-24 21:40:17 -0500

    fix: add tidyverse

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
-my_packages = c("units","leaflet","leaflet.extras", "osrm", "shinyjs", "nycgeo", "sf", "geosphere")
+my_packages = c("tidyverse","units","leaflet","leaflet.extras", "osrm", "shinyjs", "nycgeo", "sf", "geosphere")
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
-install.packages(p, dependencies = T)
+install.packages(p)
 }
 }
 invisible(sapply(my_packages, install_if_missing))
