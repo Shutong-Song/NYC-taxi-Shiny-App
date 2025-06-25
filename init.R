a55261ef028cb6ec5f9219ae9518a17d312a2378
Shutong-Song <jayssong001@gmail.com>
2025-06-24 19:56:00 -0500

    fix: add init for packages

---
+++ init.R
@@ -0,0 +1,7 @@
+my_packages = c("leaflet", "leaflet.extras", "osrm", "shinyjs", "nycgeo", "sf", "geosphere")
+install_if_missing = function(p) {
+if (p %in% rownames(installed.packages()) == FALSE) {
+install.packages(p)
+}
+}
+invisible(sapply(my_packages, install_if_missing))
