6ee9b3e3e986f98616508036fff1458b333b33f3
Shutong-Song <jayssong001@gmail.com>
2025-06-24 20:56:18 -0500

    fix: install leaflet

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
 my_packages = c("leaflet","leaflet.extras", "osrm", "shinyjs", "nycgeo", "sf", "geosphere")
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
-install.packages(p)
+install.packages(p, dependencies = T)
 }
 }
 invisible(sapply(my_packages, install_if_missing))
